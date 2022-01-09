87906b1a2eda9f6fbce09272b07678751201fa29
otmosina <otmosina@gmail.com>
2022-01-09 12:36:58 +0800

    add fizzbuzz algos

--- lib/fundamentals.go
+++ lib/fundamentals.go
@@ -1,5 +1,11 @@
 package lib
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
 func Reverse(str string) string {
 	var result []byte
 	lastIndex := len(str) - 1
@@ -8,4 +14,26 @@ func Reverse(str string) string {
 		lastIndex--
 	}
 	return string(result)
+}
+
+func FizzBuzz(start, stop int) (string, error) {
+	var result []string
+	if start > stop {
+		err := errors.New("Start > Stop. Error!")
+		return strings.Join(result, ""), err
+	}
+
+	for i := start; i <= stop; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			result = append(result, " FizzBuzz")
+		} else if i%5 == 0 {
+			result = append(result, " Buzz")
+
+		} else if i%3 == 0 {
+			result = append(result, " Fizz")
+		} else {
+			result = append(result, fmt.Sprintf(" %d", i))
+		}
+	}
+	return strings.TrimSpace(strings.Join(result, "")), nil
 }
--- lib/fundamentals_test.go
+++ lib/fundamentals_test.go
@@ -11,3 +11,26 @@ func TestReverse(t *testing.T) {
 	expected := "cba"
 	require.Equal(t, expected, Reverse(inputString))
 }
+
+func TestFizzBuzz(t *testing.T) {
+	var r string
+	var e error
+	r, e = FizzBuzz(1, 0)
+	require.NotEmpty(t, e)
+	require.Empty(t, r)
+
+	r, e = FizzBuzz(7, 7)
+
+	require.Empty(t, e)
+	require.Equal(t, "7", r)
+
+	r, e = FizzBuzz(1, 5)
+
+	require.Empty(t, e)
+	require.Equal(t, "1 2 Fizz 4 Buzz", r)
+
+	r, e = FizzBuzz(11, 20)
+
+	require.Empty(t, e)
+	require.Equal(t, "11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz", r)
+}
